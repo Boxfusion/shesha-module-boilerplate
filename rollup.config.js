2ee1a54c462d83bdcd3bfaf3102f021aa341d720
Phil Mashiane <phillemonm@boxfusion.co.za>
2021-11-18 13:39:56 +0200

    Configured rollup.config.js to allow for importing of json files in Storybooks

--- rollup.config.js
+++ rollup.config.js
@@ -7,69 +7,14 @@ import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
 import url from '@rollup/plugin-url';
 import svgr from '@svgr/rollup';
+import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
 import typescriptEngine from 'typescript';
 // import multi from '@rollup/plugin-multi-entry';
 import external from 'rollup-plugin-peer-deps-external';
 import pkg from './package.json';
 
-// export default {
-//   input: ['src/index.ts'],
-//   output: {
-//     dir: pkg.module,
-//     format: 'es',
-//     // exports: 'named',
-//   },
-//   // output: [
-//   //   // {
-//   //   //   dir: pkg.main,
-//   //   //   format: 'cjs',
-//   //   //   exports: 'named',
-//   //   // },
-//   //   {
-//   //     dir: pkg.module,
-//   //     format: 'es',
-//   //     exports: 'named',
-//   //   },
-//   // ],
-//   external: [/@babel\/runtime/],
-//   plugins: [
-//     multi(),
-//     postcss({
-//       plugins: [],
-//       minimize: true,
-//     }),
-//     peerDepsExternal({
-//       includeDependencies: true,
-//     }),
-//     typescript({
-//       typescript: typescriptEngine,
-//       include: ['*.js+(|x)', '**/*.js+(|x)'],
-//       exclude: [
-//         'coverage',
-//         'config',
-//         'dist',
-//         'node_modules/**',
-//         '*.test.{js+(|x), ts+(|x)}',
-//         '**/*.test.{js+(|x), ts+(|x)}',
-//       ],
-//     }),
-//     commonjs(),
-//     babel({
-//       extensions: [...DEFAULT_EXTENSIONS, '.ts', 'tsx'],
-//       babelHelpers: 'runtime',
-//       exclude: /node_modules/,
-//       plugins: [['@babel/plugin-transform-runtime', { useESModules: false }]],
-//     }),
-//     url(),
-//     svgr(),
-//     resolve(),
-//     terser(),
-//   ],
-// };
-
 const onwarn = (warning, warn) => {
-  console.log('warning.code: ', warning.code);
   // skip certain warnings
   if (warning.code === 'UNUSED_EXTERNAL_IMPORT') return;
 
@@ -95,6 +40,7 @@ export default {
     },
   ],
   plugins: [
+    json(),
     postcss({
       plugins: [],
       minimize: true,
